4c4d8200020dd30409e3a22c142f88a55401dddb
Dmitrij Feller <kisei.feller@gmail.com>
2016-03-13 21:20:48 +0100

    test eingefügt

---
+++ helloQuest/platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-extension": "1.3.0",
+    "cordova-plugin-admobpro": "2.12.3"
+}
+// BOTTOM OF METADATA
+});
